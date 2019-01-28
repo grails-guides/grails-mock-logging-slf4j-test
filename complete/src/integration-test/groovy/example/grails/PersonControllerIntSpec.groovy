84a003f3156803a3e1398d4b42227d9b4aac6062
niravassar <assarn@objectcomputing.com>
2019-01-28 14:20:11 -0600

    COMPLETE project grails 4 upgrade, needed to use micronaught http client.

--- complete/src/integration-test/groovy/example/grails/PersonControllerIntSpec.groovy
+++ complete/src/integration-test/groovy/example/grails/PersonControllerIntSpec.groovy
@@ -3,9 +3,12 @@ package example.grails
 
 import com.google.common.collect.ImmutableList
 import grails.gorm.transactions.Rollback
-import grails.plugins.rest.client.RestBuilder
-import grails.plugins.rest.client.RestResponse
 import grails.testing.mixin.integration.Integration
+import grails.testing.spock.OnceBefore
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.HttpClient
 import spock.lang.Shared
 import spock.lang.Specification
 import uk.org.lidalia.slf4jext.Level
@@ -20,7 +23,13 @@ class PersonControllerIntSpec extends Specification {
     @Shared
     TestLogger personControllerLogger = TestLoggerFactory.getTestLogger("example.grails.PersonController") // <1>
     @Shared
-    RestBuilder rest = new RestBuilder()
+    HttpClient client
+
+    @OnceBefore
+    void init() {
+        String baseUrl = "http://localhost:$serverPort"
+        this.client  = HttpClient.create(baseUrl.toURL())
+    }
 
     def cleanup() {
         TestLoggerFactory.clearAll() // <2>
@@ -30,11 +39,11 @@ class PersonControllerIntSpec extends Specification {
     // tag::PersonControllerIntSpecCreateSuccess[]
     void "test create person successful logs"() {
         when:"a person is created"
-        RestResponse resp = rest.get("http://localhost:${serverPort}/person/createPerson?name=Nirav&age=40")
+        HttpResponse<Map> resp = client.toBlocking().exchange(HttpRequest.GET("/person/createPerson?name=Nirav&age=40"), Map)
         ImmutableList<LoggingEvent> loggingEvents = personControllerLogger.getAllLoggingEvents() // <3>
 
         then: "check the logging events"
-        resp.status == 200
+        resp.status == HttpStatus.OK
         loggingEvents.size() == 1 // <4>
         loggingEvents[0].message == "person saved successfully: name: Nirav, age: 40" // <5>
         loggingEvents[0].level == Level.INFO
@@ -44,11 +53,11 @@ class PersonControllerIntSpec extends Specification {
     // tag::PersonControllerIntSpecCreateUnsuccess[]
     void "test create person unsuccessful logs"() {
         when:"a person is created, but has a input error"
-        RestResponse resp = rest.get("http://localhost:${serverPort}/person/createPerson?name=Bob&age=Twenty")
+        HttpResponse<Map> resp = client.toBlocking().exchange(HttpRequest.GET("/person/createPerson?name=Bob&age=Twenty"), Map)
         ImmutableList<LoggingEvent> loggingEvents = personControllerLogger.getAllLoggingEvents()
 
         then: "check the logging events"
-        resp.status == 200
+        resp.status == HttpStatus.OK
         loggingEvents.size() == 1
         loggingEvents[0].message == "Error occurred on save!"
         loggingEvents[0].level == Level.ERROR
@@ -58,15 +67,15 @@ class PersonControllerIntSpec extends Specification {
     // tag::PersonControllerIntSpecAdvice[]
     void "test offerAdvice to old person"() {
         given: "A person is already created"
-        RestResponse resp = rest.get("http://localhost:${serverPort}/person/createPerson?name=John&age=35")
+        HttpResponse<Map> resp = client.toBlocking().exchange(HttpRequest.GET("/person/createPerson?name=John&age=35"), Map)
         TestLogger ageAdvisorLogger = TestLoggerFactory.getTestLogger("example.grails.AgeAdvisor") // <1>
 
         when:"we ask for advice"
-        resp = rest.get("http://localhost:${serverPort}/person/offerAdvice?name=John")
+        resp = client.toBlocking().exchange(HttpRequest.GET("/person/offerAdvice?name=John"), Map)
         ImmutableList<LoggingEvent> loggingEvents = ageAdvisorLogger.getAllLoggingEvents()
 
         then: "check the logging events"
-        resp.status == 200
+        resp.status == HttpStatus.OK
         loggingEvents.size() == 1
         loggingEvents[0].message == "It's all downhill from here, sorry."
         loggingEvents[0].level == Level.WARN
